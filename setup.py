af423dbf19e1587d3ccf10de8d3a3dd52fbcdbb0
Mark Saxer <msaxe@users.noreply.github.com>
2017-02-26 21:23:19 -0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
+import ricecake
 from setuptools import setup
 
 setup(name='ricecake',
-      version='0.1',
-      description='Ricecooker dependent package to upload file structure to Kolibri',
-      url='http://github.com/msaxe/',
-      author='Mark Saxer',
-      author_email='masaxer@cox.net',
-      license='MIT',
-      packages=['ricecake'],
-	  install_requires=[
-          'ricecooker',
-		  'configparser',
-      ],
-      zip_safe=False)
+	version='0.1',
+	description='Ricecooker dependent package to upload file structure to Kolibri',
+	url='http://github.com/msaxe/',
+	author='Mark Saxer',
+	author_email='masaxer@cox.net',
+	license='MIT',
+	packages=['ricecake'],
+	install_requires=[
+		'ricecooker',
+		'configparser',
+	],
+	entry_points={
+		'console_scripts': [
+			'ricecake=ricecake.cli:main'
+		]
+	},
+	zip_safe=False
+)
